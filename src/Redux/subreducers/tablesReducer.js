d3e166a97d912d23fbd161e4fb4ff6fb09666a8f
Michał Mietła <mrbroomchannel@gmail.com>
2022-11-02 18:21:44 +0100

    Adjust urls in tablesReducer

--- src/Redux/subreducers/tablesReducer.js
+++ src/Redux/subreducers/tablesReducer.js
@@ -1,3 +1,5 @@
+import { API_URL } from "../../config";
+
 //selectors
 export const getAllTables = ({ tables }) => tables;
 export const getTableById = ({ tables }, tableId) => tables.find(table => table.id === tableId);
@@ -12,7 +14,7 @@ export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
 export const updateTable = payload => ({ type: UPDATE_TABLE, payload });
 export const fetchTables = loadedData => {
   return (dispatch) => {
-    fetch('http://localhost:3131/api/tables')
+    fetch(`${API_URL}/tables`)
       .then(res => res.json())
       .then(tables => dispatch(updateTables(tables)))
       .then(() => loadedData(true));
@@ -35,7 +37,7 @@ export const updateTableRequest = table => {
       })
     };
 
-    fetch(`http://localhost:3131/api/tables/${table.id}`, options)
+    fetch(`${API_URL}/tables/${table.id}`, options)
       .then(() => dispatch(updateTable(table)));
   }
 }
